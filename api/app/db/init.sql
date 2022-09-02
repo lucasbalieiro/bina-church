7c40a4ca24f118c23107f5d20fd946430467d122
Lucas Balieiro <lucasbalieirom@hotmail.com>
2022-09-01 20:24:54 -0400

    feat: add new database for dev environment

--- api/app/db/init.sql
+++ api/app/db/init.sql
@@ -1,5 +1,8 @@
 SELECT 'CREATE DATABASE application'
 WHERE NOT EXISTS (SELECT FROM pg_database WHERE datname = 'application')\gexec
 
+SELECT 'CREATE DATABASE development'
+WHERE NOT EXISTS (SELECT FROM pg_database WHERE datname = 'development')\gexec
+
 SELECT 'CREATE DATABASE test_application'
 WHERE NOT EXISTS (SELECT FROM pg_database WHERE datname = 'test_application')\gexec
